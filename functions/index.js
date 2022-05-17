08c640cd8ac430f278aae56fac77b9a81eed21c0
Sam McGrail <sammcgrail@gmail.com>
2022-05-17 02:04:06 -0400

    add console in other spot

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,7 @@ const functions = require("firebase-functions");
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
+  console.log("sup from helloWorld function");
 });
 
 console.log("sup");
